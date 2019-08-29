6b142e3da341152ff74ea80e725889231c515079
MrJane <523093962@qq.com>
2019-08-29 08:49:24 +0800

    conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="home">
-<<<<<<< HEAD
+
         <mine-drawer></mine-drawer>
-=======
         <mine-table style="margin-left: 300px" :columns="columns" :data="dataSource"></mine-table>
->>>>>>> 32dfda188443ba52065ba714c7138ecc871d09f1
+
         <!--        <mine-card v-model="single" class="box-card">-->
         <!--            <mine-button size="large" type="primary" class="primary">大号按钮</mine-button>-->
         <!--            <mine-button>默认按钮</mine-button>-->
@@ -178,13 +177,13 @@
                 <div>固定测试组件</div>
             </mine-affix>
             <p v-for="num in 20">测试</p>
-<<<<<<< HEAD
+
             <mine-back-top>
 
             </mine-back-top>
-=======
+
             <p v-for="num in 30">测试</p>
->>>>>>> 32dfda188443ba52065ba714c7138ecc871d09f1
+
         </div>
 
     </div>
@@ -211,16 +210,13 @@
   import MinePager from '../components/pager/pager';
   import MineSwiperItem from '../components/swiper/swiper-item'
   import MineSwiper from '../components/swiper/swiper';
-<<<<<<< HEAD
   import MineAffix from '../components/affix/affix'
   import MineCarousel from '../components/carousel/carousel';
   import MineCarouselItem from '../components/carousel/carousel-item';
   import MineBackTop from '../components/back-top/back-top';
   import MineDrawer from '../components/drawer/drawer';
-=======
-  import MineAffix from '../components/affix/affix';
   import MineTable from '../components/table/table'
->>>>>>> 32dfda188443ba52065ba714c7138ecc871d09f1
+
 
 
   export default {
@@ -293,14 +289,12 @@
       MineSwiper,
       MineSwiperItem,
       MineAffix,
-<<<<<<< HEAD
       MineCarousel,
       MineCarouselItem,
       MineBackTop,
-      MineDrawer
-=======
+      MineDrawer,
       MineTable
->>>>>>> 32dfda188443ba52065ba714c7138ecc871d09f1
+
     },
     methods: {
       handlerChange(status) {
